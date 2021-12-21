7614ac3f6b8375e89e65659d03590b18d1d2528f
大巻次郎 <Jiro@oomakijirou-no-MacBook-Pro.local>
2021-12-21 21:24:08 +0900

    [課題] 名無し判定

--- omikuji/omikuji.js
+++ omikuji/omikuji.js
@@ -2,6 +2,9 @@ let username;
 let userresult;
 
 username = prompt("お名前を教えて下さい。");
+if (username == "") {
+  username = "名無し";
+}
 document.getElementById("name").innerHTML = username;
 
 let rand = Math.floor( Math.random() * 5);
